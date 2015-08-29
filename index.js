bd709cc12b19e7ef20480bc92044c5cb18aef450
fonorobert <fonorobert@gmail.com>
2015-08-30 00:55:28 +0200

    build basic statistics

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var tumblr = require('tumblr.js');
 var fs = require('fs');
 var postProcess = require('./lib/postprocess.js');
+var stats = require('./lib/stats.js');
 
 var blogName = process.argv[2];
 var blogUrl = blogName + 'tumblr.com';
-var reFetch = process.argv[3] ? process.argv[3] : false;
+var reFetch = process.argv[4] === true ? true : false;
+var postNumber = Number(process.argv[3]) ? Number(process.argv[3]) : 0;
+console.log(Number(process.argv[3]));
 var fileName = 'res/' + blogName + '.json';
 var tag = "bestiary";
 
@@ -18,13 +21,15 @@ if(!fs.existsSync(fileName) || reFetch) {
     var posts = resp.posts;
     fs.writeFile(fileName, JSON.stringify(posts), function(err) {
       if(err) throw err;
-      console.log('saved file');
+      console.log('Successfully saved file');
     });
+    console.log(stats.basic(posts, postNumber));
   })
 } else {
   fs.readFile(fileName, function(err, data){
     if(err) throw err;
     var posts = JSON.parse(data);
-    console.log('read file');
+    console.log('Successfully read file');
+    console.log(stats.basic(posts, postNumber));
   })
 }
